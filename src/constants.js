a9588c2cd2797a76013f554283420928f7f157c9
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2022-03-12 10:08:26 +0200

    Add the second API (API2) with its apiKey2.

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,9 @@ const constants = {
     apiKey: 'ca3d69ee336e43d8099727f0d7ce3859',
     imageBaseUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face',
     defaultPoster: 'https://www.reelviews.net/resources/img/default_poster.jpg',
+    baseUrl2: 'http://www.omdbapi.com/',
+    // i: 'tt3896198',
+    // s: 'Matrix',
+    apiKey2: 'e4c2f18d',
 }
 export default constants;
